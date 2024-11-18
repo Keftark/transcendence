a2b027c6f964925c68b82201a2e2cd844a429537
MartinJuette <m.juette76@gmail.com>
2024-11-18 15:40:46 +0100

    accès à l'admin de django + rest framework activé

--- ping_pong/settings.py
+++ ping_pong/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['localhost']
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -74,6 +75,19 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'ping_pong.wsgi.application'
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication'
+    ),
+}
+
 
 
 # Database
--- ping_pong/urls.py
+++ ping_pong/urls.py
@@ -14,10 +14,25 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib.auth.models import User
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework import routers, serializers, viewsets
+from polls.serializers import GroupSerializer, UserSerializer
+
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+# Routers provide a way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("polls.urls")),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
