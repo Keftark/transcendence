86fd7678d253d9ce171785a7d7555feeb4f9cae6
keftark <dev.christian.p@gmail.com>
2024-11-20 12:44:35 +0100

    working on menu anims

--- static/website/js/modesSelection.js
+++ static/website/js/modesSelection.js
@@ -7,23 +7,39 @@ import { playerStats } from './playerManager.js';
 
 const modesLocalButton = document.getElementById('modesLocalButton');
 const modesOnlineButton = document.getElementById('modesOnlineButton');
+const modeDuelButton = document.getElementById('modeDuelButton');
+const modeLocalButton = document.getElementById('modeLocalButton');
+const modeComputerButton = document.getElementById('modeComputerButton');
+const modeTournamentButton = document.getElementById('modeTournamentButton');
 
-document.getElementById('modeLocalButton').addEventListener('click', () => {
+modeLocalButton.addEventListener('click', () => {
     selectedMode = LevelMode.LOCAL;
-    openRules();
+    closeLocalModes();
+    setTimeout(() => {
+        openRules();
+    }, 300);
 });
 
-document.getElementById('modeComputerButton').addEventListener('click', () => {
+modeComputerButton.addEventListener('click', () => {
     selectedMode = LevelMode.ADVENTURE;
-    openRules();
+    closeLocalModes();
+    setTimeout(() => {
+        openRules();
+    }, 300);
 });
 
-document.getElementById('modeDuelButton').addEventListener('click', () => {
-    openDuelPanel();
+modeDuelButton.addEventListener('click', () => {
+    closeOnlineModes();
+    setTimeout(() => {
+        openDuelPanel();
+    }, 300);
 });
 
-document.getElementById('modeTournamentButton').addEventListener('click', () => {
-    openTournamentMenu();
+modeTournamentButton.addEventListener('click', () => {
+    closeOnlineModes();
+    setTimeout(() => {
+        openTournamentMenu();
+    }, 300);
 });
 
 document.getElementById('createTournamentButton').addEventListener('click', () => {
@@ -50,6 +66,8 @@ modesOnlineButton.addEventListener('click', () => {
 let isInsideModes = false;
 let selectedMode;
 let animReverse = false;
+let isLocalModes = false;
+let isOnlineModes = false;
 
 export function clickPlayGame()
 {
@@ -77,6 +95,7 @@ function resetBigModesAnim()
 function closeInsideModes()
 {
     isInsideModes = false;
+    
     document.getElementById('modesLocal').style.display = 'none';
     document.getElementById('modesOnline').style.display = 'none';
     document.getElementById('modesSelectionParent').style.display = 'flex';
@@ -94,17 +113,22 @@ export function showModeChoice()
 
 function clickModesLocal()
 {
+    isLocalModes = true;
     closeBigModes();
     setTimeout(() => {
         isInsideModes = true;
         document.getElementById('modesSelectionParent').style.display = 'none';
         document.getElementById('modesLocal').style.display = 'flex';
-        document.getElementById('modeLocalButton').focus();
+        modeLocalButton.focus();
+        modeLocalButton.style.animationDirection = 'normal';
+        modeComputerButton.style.animationDirection = 'normal';
+        animLocalModes();
     }, 300);
 }
 
 function clickModesOnline()
 {
+    isOnlineModes = true;
     if (!playerStats.isRegistered)
         return;
     closeBigModes();
@@ -112,8 +136,58 @@ function clickModesOnline()
         isInsideModes = true;
         document.getElementById('modesSelectionParent').style.display = 'none';
         document.getElementById('modesOnline').style.display = 'flex';
-        document.getElementById('modeDuelButton').focus();
+        modeDuelButton.focus();
+        animOnlineModes();
     }, 300);
+}
+
+function animLocalModes()
+{
+    modeLocalButton.classList.add('littleAppearAnim');
+    modeComputerButton.classList.add('littleAppearAnim');
+}
+
+function resetLocalAnim()
+{
+    modeLocalButton.classList.remove('littleAppearAnim');
+    modeComputerButton.classList.remove('littleAppearAnim');
+    void modeLocalButton.offsetWidth;
+    void modeComputerButton.offsetWidth;
+    animLocalModes();
+}
+
+function closeLocalModes()
+{
+    modeLocalButton.style.animationDirection = 'reverse';
+    modeComputerButton.style.animationDirection = 'reverse';
+    resetLocalAnim();
+}
+
+function resetOnlineAnim()
+{
+    animReverse = false;
+    modeDuelButton.style.animationDirection = 'normal';
+    modeTournamentButton.style.animationDirection = 'normal';
+}
+
+function animOnlineModes()
+{
+    modeDuelButton.classList.add('littleAppearAnim');
+    modeTournamentButton.classList.add('littleAppearAnim');
+}
+
+function closeOnlineModes()
+{
+    isOnlineModes = false;
+    modeDuelButton.classList.remove('littleAppearAnim');
+    modeTournamentButton.classList.remove('littleAppearAnim');
+    void modeDuelButton.offsetWidth;
+    void modeTournamentButton.offsetWidth;
+    modeDuelButton.style.animationDirection = animReverse ? 'normal' : 'reverse';
+    modeTournamentButton.style.animationDirection = animReverse ? 'normal' : 'reverse';
+    animReverse = !animReverse;
+    resetOnlineAnim();
+    animOnlineModes();
 }
 
 function animBigModes()
@@ -145,7 +219,15 @@ export function onModesOpen()
 export function clickBackButtonMenu()
 {
     if (isInsideModes)
-        closeInsideModes();
+    {
+        if (isLocalModes)
+            closeLocalModes();
+        else if (isOnlineModes)
+            closeOnlineModes();
+        setTimeout(() => {
+            closeInsideModes();
+        }, 300);
+    }
     else
     {
         closeBigModes();
