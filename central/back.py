c0ceb128f0309cd7e8bb49c97df2e9aa25ea0ee8
Nolan <nolan.xx.mcfr@gmail.com>
2025-01-15 15:29:03 +0100

    Correction

--- central/back.py
+++ central/back.py
@@ -78,43 +78,21 @@ async def handle_answers(websocket, event, message):
                         await user.send(json.dumps(data))
                     except Exception as e:
                         logger.log("", 2, e)
-                    if data["type"] == "match_init":
-                        user.room_id = (int)(data["room_id"])
-                    elif data["type"] == "join_queue":
-                        user.status = "1v1_classic"
+                    if data["type"] == "join_queue":
+                        user.game = "1v1_classic"
+                        user.status = "queue"
                         user.room_id = -1
                     elif data["type"] == "exit_queue":
-                        user.status = "1v1_classic"
+                        user.game = "none"
+                        user.status = "here"
                         user.room_id = -1
-        #if type == "match_init":
-        #    for user in userList:
-        #        print("Checking user with ID :", user.id, "\tComparing with IDs", event["id_p1"], "and", event["id_p2"])
-        #        if user.id == event["id_p1"] or user.id == event["id_p2"]:
-        #            print("Found user !")
-        #            user.room_id = (int)(event["room_id"])
-        #if "room_id" in event: #transmit to all user in room_id
-        #    print("Sending to room id")
-        #    for user in userList:
-        #        print("Checking user with ID :", user.id, "\tIn room ID :", user.room_id, "in game", user.status)
-        #        if user.status == "1v1_classic" and user.room_id == (int)(event["room_id"]):
-        #            print("Found corresponding room !")
-        #            await user.send(json.dumps(event))
-        #elif type == "join_queue":
-        #    id = (int)(event["id"])
-        #    for user in userList:
-        #        if user.id == id:
-        #            user.status = "1v1_classic"
-        #            user.room_id = -1
-        #            break
-        #elif type == "exit_queue":
-        #    id = (int)(event["id"])
-        #    for user in userList:
-        #        if user.id == id:
-        #            user.status = "here"
-        #            user.room_id = -1
-        #            break
-        #else:
-        #    pass #transfer to room id
+                    elif data["type"] == "match_init":
+                        user.status = "playing"
+                        user.room_id = (int)(data["room_id"])
+                    elif data["type"] == "victory":
+                        user.game = "none"
+                        user.status = "here"
+                        user.room_id = -1
     elif type == "message" or type == "sticker":
         id = (int)(event["id"])
         for user in userList:
@@ -218,6 +196,7 @@ async def handler(websocket):
         await websocket.wait_closed()
     except Exception as e:
         logger.log("", 2, e)
+        print("GOTT ::", message)
     finally:
         #TODO : Disconnection procedure
         if websocket == chat_socket:
