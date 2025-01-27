ba0fd9f29a711ac51f3c136af77364f5a4b21d99
Nolan <nolan.xx.mcfr@gmail.com>
2025-01-27 15:15:48 +0100

    GET LINTED

--- back_1v1/Game/obstacle.py
+++ back_1v1/Game/obstacle.py
@@ -1,108 +1,186 @@
-from ball import Ball
+"""OBSTACLE"""
 
 PRECISION = 10
 
-class Obstacle:
-    #define a square obstacle. 
+class Obstacle():
+    """Defines an obstacle.
+    An obstacle can bounce a ball, and be
+    destroyed if needed.
+    """
     def __init__(self, x, y, size, life = -1, color = 0x000000):
-        self._x = x 
+        self._x = x
         self._y = y
         self._size = size
         self._life = life
         self._color = color
-        self._breakable = True if life > 0 else False
+        self._breakable = life > 0
 
     def upper_bound(self):
-        return (self._y)
-    
+        """Returns the upper bound of the obstacle.
+
+        Returns:
+            float: y of the upper bound.
+        """
+        return self._y
+
     def upper_collision(self, ball):
+        """Checks the upper side collision of the obstacle.
+
+        Args:
+            ball (Ball): the ball.
+
+        Returns:
+            bool: `True` if the ball collides, `False` otherwise.
+        """
         current_y = ball.y
-        next_y = ball.y + ball.velocity_y if ball.is_powered_up is False else ball.velocity_boosted_y
+        next_y = ball.y + ball.velocity_y if ball.is_powered_up is False \
+                else ball.velocity_boosted_y
         if current_y > next_y:
             current_y, next_y = next_y, current_y
         for i in range((int)(current_y * PRECISION), (int)(next_y * PRECISION), 1):
-            if (i / PRECISION >= self.upper_bound() and i / PRECISION <= self.upper_bound() + self._size):
+            if (i / PRECISION >= self.upper_bound()
+                and i / PRECISION <= self.upper_bound() + self._size):
                 return True
         return False
 
     def lower_bound(self):
-        return (self._y - self._size)
-    
+        """Returns the lower bound of the obstacle.
+
+        Returns:
+            float: y of the lowe bound.
+        """
+        return self._y - self._size
+
     def lower_collision(self, ball):
+        """Checks the lower side collision of the obstacle.
+
+        Args:
+            ball (Ball): the ball.
+
+        Returns:
+            bool: `True` if the ball collides, `False` otherwise.
+        """
         current_y = ball.y
-        next_y = ball.y + ball.velocity_y if ball.is_powered_up is False else ball.velocity_boosted_y
+        next_y = ball.y + ball.velocity_y if ball.is_powered_up is False \
+                else ball.velocity_boosted_y
         if current_y > next_y:
             current_y, next_y = next_y, current_y
         for i in range((int)(current_y * PRECISION), (int)(next_y * PRECISION), 1):
-            if (i / PRECISION <= self.lower_bound() and i / PRECISION >= self.lower_bound() - self._size):
+            if (i / PRECISION <= self.lower_bound()
+                and i / PRECISION >= self.lower_bound() - self._size):
                 return True
         return False
-    
+
     def right_bound(self):
-        return (self._x + self._size)
-    
+        """Returns the right bound of the obstacle.
+
+        Returns:
+            float: x of the right bound.
+        """
+        return self._x + self._size
+
     def right_collision(self, ball):
+        """Checks the right side collision of the obstacle.
+
+        Args:
+            ball (Ball): the ball.
+
+        Returns:
+            bool: `True` if the ball collides, `False` otherwise.
+        """
         current_x = ball.x
-        next_x = ball.x + ball.velocity_x if ball.is_powered_up is False else ball.velocity_boosted_x
+        next_x = ball.x + ball.velocity_x if ball.is_powered_up is False \
+                else ball.velocity_boosted_x
         if current_x > next_x:
             current_x, next_x = next_x, current_x
         for i in range((int)(current_x * PRECISION), (int)(next_x * PRECISION), 1):
-            if (i / PRECISION >= self.right_bound() and i / PRECISION <= self.right_bound() + self._size):
+            if (i / PRECISION >= self.right_bound()
+                and i / PRECISION <= self.right_bound() + self._size):
                 return True
         return False
-    
+
     def left_bound(self):
-        return (self._x)
-    
+        """Returns the left bound of the obstacle.
+
+        Returns:
+            float: x of the left bound.
+        """
+        return self._x
+
     def left_collision(self, ball):
+        """Checks the left side collision of the obstacle.
+
+        Args:
+            ball (Ball): the ball.
+
+        Returns:
+            bool: `True` if the ball collides, `False` otherwise.
+        """
         current_x = ball.x
-        next_x = ball.x + ball.velocity_x if ball.is_powered_up is False else ball.velocity_boosted_x
+        next_x = ball.x + ball.velocity_x if ball.is_powered_up is False \
+                else ball.velocity_boosted_x
         if current_x > next_x:
             current_x, next_x = next_x, current_x
         for i in range((int)(current_x * PRECISION), (int)(next_x * PRECISION), 1):
-            if (i / PRECISION <= self.left_bound() and i / PRECISION >= self.left_bound() - self._size):
+            if (i / PRECISION <= self.left_bound()
+                and i / PRECISION >= self.left_bound() - self._size):
                 return True
         return False
-    
-    #checks if the ball is colliding with the obstacle. If hurts is
-    #set to true, remove one life from the obstacle. 
+
     def collide(self, ball, hurts = False):
+        """checks if the ball is colliding with the obstacle. If hurts is
+        set to true, remove one life from the obstacle.
+
+        Args:
+            ball (Ball): The ball.
+            hurts (bool, optional): Wether or not the bounce should
+            remove life from the obstacle. Defaults to False.
+        """
         if self.right_collision(ball): #right side collision
-            for y in range((int)(self.lower_bound() * PRECISION), (int)(self.upper_bound() * PRECISION), 1):
+            for y in range((int)(self.lower_bound() * PRECISION),
+                           (int)(self.upper_bound() * PRECISION), 1):
                 y_pos = y / PRECISION
                 if ball.is_colliding(self.right_bound(), y_pos):
                     ball.bounce_vertical()
-                    if (hurts is True):
+                    if hurts is True:
                         self._life -= 1
                     break
         elif self.left_collision(ball): #left side collision
-            for y in range((int)(self.lower_bound() * PRECISION), (int)(self.upper_bound() * PRECISION), 1):
+            for y in range((int)(self.lower_bound() * PRECISION),
+                           (int)(self.upper_bound() * PRECISION), 1):
                 y_pos = y / PRECISION
                 if ball.is_colliding(self.left_bound(), y_pos):
                     ball.bounce_vertical()
-                    if (hurts is True):
+                    if hurts is True:
                         self._life -= 1
                     break
         elif self.upper_collision(ball): #top collision
-            for x in range((int)(self.left_bound() * PRECISION), (int)(self.right_bound() * PRECISION), 1):
+            for x in range((int)(self.left_bound() * PRECISION),
+                           (int)(self.right_bound() * PRECISION), 1):
                 x_pos = x / PRECISION
                 if ball.is_colliding(x_pos, self.upper_bound()):
                     ball.bounce_horizontal()
-                    if (hurts is True):
+                    if hurts is True:
                         self._life -= 1
-                    break 
+                    break
         elif self.lower_collision(ball): #bottom collision
-            for x in range((int)(self.left_bound() * PRECISION), (int)(self.right_bound() * PRECISION), 1):
+            for x in range((int)(self.left_bound() * PRECISION),
+                           (int)(self.right_bound() * PRECISION), 1):
                 x_pos = x / PRECISION
                 if ball.is_colliding(x_pos, self.lower_bound()):
                     ball.bounce_horizontal()
-                    if (hurts is True):
+                    if hurts is True:
                         self._life -= 1
                     break
 
     #getters and setters
     @property
     def x(self):
+        """Returns the upper left x position of the obstacle.
+
+        Returns:
+            float: x position.
+        """
         return self._x
 
     @x.setter
@@ -111,6 +189,11 @@ class Obstacle:
 
     @property
     def y(self):
+        """Returns the upper left y position of the obstacle.
+
+        Returns:
+            float: y position.
+        """
         return self._y
 
     @y.setter
@@ -119,6 +202,11 @@ class Obstacle:
 
     @property
     def size(self):
+        """Returns the size of the obstacle.
+
+        Returns:
+            int: size of the obstacle. Defaults to 1.
+        """
         return self._size
 
     @size.setter
@@ -127,6 +215,11 @@ class Obstacle:
 
     @property
     def life(self):
+        """Returns the hit points of the obstacle.
+
+        Returns:
+            int: the life of the obstacle.
+        """
         return self._life
 
     @life.setter
@@ -135,6 +228,11 @@ class Obstacle:
 
     @property
     def color(self):
+        """Returns the color of the obstacle.
+        
+        Returns:
+            int: hexadecimal color value.
+        """
         return self._color
 
     @color.setter
@@ -143,6 +241,11 @@ class Obstacle:
 
     @property
     def breakable(self):
+        """Returns wether or not the obstacle is breakable.
+
+        Returns:
+            bool: `True` if the obstacle is breakable, `False` otherwise.
+        """
         return self._breakable
 
     @breakable.setter
--- back_1v1/Game/paddle.py
+++ back_1v1/Game/paddle.py
@@ -1,4 +1,4 @@
-""""e"""
+""""Paddle"""
 
 PRECISION = 15
 DEFAULT_SPEED = 0.75
@@ -7,12 +7,14 @@ MOVE_DOWN  = -1
 MOVE_NONE = 0
 
 class Paddle:
-    def __init__(self, id, x, len, board_top_y, board_bottom_y):
-        self._id = id
+    """A paddle, the player character.
+    """
+    def __init__(self, _id, x, _len, board_top_y, board_bottom_y):
+        self._id = _id
         self._x = x
         self._y = 0
         self._width = 0.5
-        self._length = len
+        self._length = _len
         self._speed = DEFAULT_SPEED
         self._y_max = board_top_y
         self._y_min = board_bottom_y
@@ -28,8 +30,13 @@ class Paddle:
         self._speed_bonus = 0
         self._power_bonus = 0
         self._bounce_bonus = 1
-    
+
     def set_up(self, payload):
+        """Loads optionnal parameters.
+
+        Args:
+            payload (dict): contains optional parameters.
+        """
         for data in payload:
             match data:
                 case "length":
@@ -42,86 +49,170 @@ class Paddle:
                     self._bounce_bonus = (int)(payload[data])
 
     def upper_bound(self):
-        return (self._y + self.length / 2)
-    
+        """Returns the upper bound of the paddle.
+
+        Returns:
+            float: upper y of the paddle.
+        """
+        return self._y + self.length / 2
+
     def upper_collision(self, ball):
+        """Check wether the ball is colliding with the upper 
+        part of the paddle or not.
+
+        Args:
+            ball (Ball): A ball.
+
+        Returns:
+            bool: `True` if the ball collides, `False` otherwise.
+        """
         current_y = ball.y
-        next_y = ball.y + ball.velocity_y if ball.is_powered_up is False else ball.velocity_boosted_y
+        next_y = ball.y + ball.velocity_y if ball.is_powered_up is False \
+                else ball.velocity_boosted_y
         if current_y > next_y:
             current_y, next_y = next_y, current_y
         for i in range((int)(current_y * PRECISION), (int)(next_y * PRECISION), 1):
-            if (i / PRECISION >= self.upper_bound() and i / PRECISION <= self.upper_bound() + self._width):
+            if (i / PRECISION >= self.upper_bound()
+                and i / PRECISION <= self.upper_bound() + self._width):
                 return True
         return False
 
     def lower_bound(self):
-        return (self._y - self.length / 2)
-    
+        """Returns the lower bound of the paddle.
+
+        Returns:
+            float: lower y of the paddle.
+        """
+        return self._y - self.length / 2
+
     def lower_collision(self, ball):
+        """Check wether the ball is colliding with the lower 
+        part of the paddle or not.
+
+        Args:
+            ball (Ball): A ball.
+
+        Returns:
+            bool: `True` if the ball collides, `False` otherwise.
+        """
         current_y = ball.y
-        next_y = ball.y + ball.velocity_y if ball.is_powered_up is False else ball.velocity_boosted_y
+        next_y = ball.y + ball.velocity_y if ball.is_powered_up is False \
+                else ball.velocity_boosted_y
         if current_y > next_y:
             current_y, next_y = next_y, current_y
         for i in range((int)(current_y * PRECISION), (int)(next_y * PRECISION), 1):
-            if (i / PRECISION <= self.lower_bound() and i / PRECISION >= self.lower_bound() - self._width):
+            if (i / PRECISION <= self.lower_bound()
+                and i / PRECISION >= self.lower_bound() - self._width):
                 return True
         return False
-    
+
     def right_bound(self):
-        return (self._x + self._width)
-    
+        """Returns the right bound of the paddle.
+
+        Returns:
+            float: right x of the paddle.
+        """
+        return self._x + self._width
+
     def right_collision(self, ball):
+        """Check wether the ball is colliding with the right 
+        part of the paddle or not.
+
+        Args:
+            ball (Ball): A ball.
+
+        Returns:
+            bool: `True` if the ball collides, `False` otherwise.
+        """
         current_x = ball.x
-        next_x = ball.x + ball.velocity_x if ball.is_powered_up is False else ball.velocity_boosted_x
+        next_x = ball.x + ball.velocity_x if ball.is_powered_up is False \
+                else ball.velocity_boosted_x
         if current_x > next_x:
             current_x, next_x = next_x, current_x
         for i in range((int)(current_x * PRECISION), (int)(next_x * PRECISION), 1):
-            if (i / PRECISION >= self.right_bound() and i / PRECISION <= self.right_bound() + self._width):
+            if (i / PRECISION >= self.right_bound()
+                and i / PRECISION <= self.right_bound() + self._width):
                 return True
         return False
-    
+
     def left_bound(self):
-        return (self._x)
-    
+        """Returns the left bound of the paddle.
+
+        Returns:
+            float: left x of the paddle.
+        """
+        return self._x
+
     def left_collision(self, ball):
+        """Check wether the ball is colliding with the left 
+        part of the paddle or not.
+
+        Args:
+            ball (Ball): A ball.
+
+        Returns:
+            bool: `True` if the ball collides, `False` otherwise.
+        """
         current_x = ball.x
-        next_x = ball.x + ball.velocity_x if ball.is_powered_up is False else ball.velocity_boosted_x
+        next_x = ball.x + ball.velocity_x if ball.is_powered_up is False \
+                else ball.velocity_boosted_x
         if current_x > next_x:
             current_x, next_x = next_x, current_x
         for i in range((int)(current_x * PRECISION), (int)(next_x * PRECISION), 1):
-            if (i / PRECISION <= self.left_bound() and i / PRECISION >= self.left_bound() - self._width):
+            if (i / PRECISION <= self.left_bound()
+                and i / PRECISION >= self.left_bound() - self._width):
                 return True
         return False
-    
+
     def move_up(self):
-        if (self.upper_bound() < self._y_max):
+        """Displaces the paddle up.
+        """
+        if self.upper_bound() < self._y_max:
             self.y += self._speed + self._speed_bonus
-    
+
     def move_down(self):
-        if (self.lower_bound() > self._y_min):
+        """Displaces the paddle down.
+        """
+        if self.lower_bound() > self._y_min:
             self.y -= self._speed + self._speed_bonus
 
     def set_move_up(self):
+        """Sets the movement of the paddle upwards.
+        """
         self._vertical = MOVE_UP
 
     def set_move_down(self):
+        """Sets the movement of the paddle downwards.
+        """
         self._vertical = MOVE_DOWN
 
     def set_move_stop(self):
+        """Stops the movement of the paddle.
+        """
         self._vertical = MOVE_NONE
 
     def tick(self):
+        """Ticks the paddle, moving it if needed.
+        """
         if self._vertical == MOVE_DOWN:
             self.move_down()
         elif self._vertical == MOVE_UP:
             self.move_up()
 
     def reset(self):
-        self._ready = False 
+        """Resets the paddle's position and ready status.
+        """
+        self._ready = False
         self._vertical = MOVE_NONE
         self._y = 0
 
     def charge_power(self, ball):
+        """Transfers the power to the ball if the paddle
+        is set as such, gain power otherwise.
+
+        Args:
+            ball (Ball): ball to charge up.
+        """
         self._power += self._power_per_bounce
         if self._power > 100:
             self._power = 100
@@ -130,62 +221,85 @@ class Paddle:
             ball.activate_power_up(self._power_multiplier + self._power_bonus)
 
     def boosto(self):
-        if (self._power >= 100):
+        """Activates the power-up.
+        """
+        if self._power >= 100:
             self._power = 0
             self._is_powered_up = True
-    
+
     #checks if the ball is colliding with the paddle.
     def collide(self, ball):
+        """Checks wether the paddle collides with the ball or not.
+
+        Args:
+            ball (Ball): The colliding ball.
+        """
         if self.right_collision(ball): #right side collision
-            for y in range((int)(self.lower_bound() * PRECISION), (int)(self.upper_bound() * PRECISION), 1):
+            for y in range((int)(self.lower_bound() * PRECISION),
+                           (int)(self.upper_bound() * PRECISION), 1):
                 y_pos = y / PRECISION
                 if ball.is_colliding(self.right_bound(), y_pos):
                     ball.bounce_paddle_left(self._y, self._length, self._bounce_bonus)
                     self.charge_power(ball)
                     break
         elif self.left_collision(ball): #left side collision
-            for y in range((int)(self.lower_bound() * PRECISION), (int)(self.upper_bound() * PRECISION), 1):
+            for y in range((int)(self.lower_bound() * PRECISION),
+                           (int)(self.upper_bound() * PRECISION), 1):
                 y_pos = y / PRECISION
                 if ball.is_colliding(self.left_bound(), y_pos):
                     ball.bounce_paddle_right(self._y, self._length, self._bounce_bonus)
                     self.charge_power(ball)
                     break
         elif self.upper_collision(ball): #top collision
-            for x in range((int)(self.left_bound() * PRECISION), (int)(self.right_bound() * PRECISION), 1):
+            for x in range((int)(self.left_bound() * PRECISION),
+                           (int)(self.right_bound() * PRECISION), 1):
                 x_pos = x / PRECISION
                 if ball.is_colliding(x_pos, self.upper_bound()):
                     ball.bounce_horizontal()
                     self.charge_power(ball)
-                    break 
+                    break
         elif self.lower_collision(ball): #bottom collision
-            for x in range((int)(self.left_bound() * PRECISION), (int)(self.right_bound() * PRECISION), 1):
+            for x in range((int)(self.left_bound() * PRECISION),
+                           (int)(self.right_bound() * PRECISION), 1):
                 x_pos = x / PRECISION
                 if ball.is_colliding(x_pos, self.lower_bound()):
                     ball.bounce_horizontal()
                     self.charge_power(ball)
                     break
 
-    def input_move(self, input):
-        if (input["move"] == "down"):
-            if (input["method"] == "held"):
+    def input_move(self, data):
+        """Parses the input, and acts
+        if needed.
+
+        Args:
+            data (dict): data to parse.
+        """
+        if data["move"] == "down":
+            if data["method"] == "held":
                 self.set_move_down()
-            elif (input["method"] == "release"):
+            elif data["method"] == "release":
                 self.set_move_stop()
             else:
                 self.move_down()
-        elif (input["move"] == "up"):
-            if (input["method"] == "held"):
+        elif data["move"] == "up":
+            if data["method"] == "held":
                 self.set_move_up()
-            elif (input["method"] == "release"):
+            elif data["method"] == "release":
                 self.set_move_stop()
             else:
                 self.move_up()
-        elif (input["move"] == "boost"):
+        elif data["move"] == "boost":
             self.boosto()
 
     #Setters and getters
     @property
     def id(self):
+        """returns the ID of the paddle, corresponding
+        to the user.
+
+        Returns:
+            int: ID of the paddle.
+        """
         return self._id
 
     @id.setter
@@ -194,6 +308,11 @@ class Paddle:
 
     @property
     def x(self):
+        """Returns the x position of the paddle's center.
+
+        Returns:
+            float: x position of the center.
+        """
         return self._x
 
     @x.setter
@@ -202,6 +321,11 @@ class Paddle:
 
     @property
     def y(self):
+        """Returns the y position of the paddle's center.
+
+        Returns:
+            float: y position of the center.
+        """
         return self._y
 
     @y.setter
@@ -210,6 +334,11 @@ class Paddle:
 
     @property
     def width(self):
+        """Returns the width of the paddle.
+
+        Returns:
+            int: width of the paddle.
+        """
         return self._width
 
     @width.setter
@@ -218,6 +347,11 @@ class Paddle:
 
     @property
     def length(self):
+        """Returns the length of the paddle.
+
+        Returns:
+            float: lenght of the paddle.
+        """
         return self._length
 
     @length.setter
@@ -226,14 +360,25 @@ class Paddle:
 
     @property
     def speed(self):
+        """returns the speed of the paddle.
+
+        Returns:
+            float: speed
+        """
         return self._speed
 
     @speed.setter
     def speed(self, value):
         self._speed = value
-    
+
     @property
     def y_max(self):
+        """Returns the y max reachable by the paddle, corresponding
+        to the top of the board.
+
+        Returns:
+            int: max y reachable.
+        """
         return self._y_max
 
     @y_max.setter
@@ -242,14 +387,25 @@ class Paddle:
 
     @property
     def y_min(self):
+        """Returns the y min reachable by the paddle, corresponding
+        to the bottom of the board.
+
+        Returns:
+            int: min y reachable.
+        """
         return self.y_min
 
-    @y_max.setter
+    @y_min.setter
     def y_min(self, value):
         self.y_min = value
 
     @property
     def vertical(self):
+        """Returns the vertical movement input.
+
+        Returns:
+            string: movement descriptor.
+        """
         return self._vertical
 
     @vertical.setter
@@ -257,15 +413,12 @@ class Paddle:
         self._vertical = value
 
     @property
-    def horizont(self):
-        return self._horizont
-
-    @horizont.setter
-    def horizont(self, value):
-        self._horizont = value
-
-    @property
     def ready(self):
+        """Returns the ready state of the paddle.
+
+        Returns:
+            bool: `True` if the paddle is ready, `False` otherwise.
+        """
         return self._ready
 
     @ready.setter
@@ -274,6 +427,12 @@ class Paddle:
 
     @property
     def power(self):
+        """Returns the power of the paddle, needed to
+        activate the powerup.
+
+        Returns:
+            int: power.
+        """
         return self._power
 
     @power.setter
@@ -282,6 +441,11 @@ class Paddle:
 
     @property
     def power_per_bounce(self):
+        """Returns how much power is gained per bounce.
+
+        Returns:
+            float: power gained.
+        """
         return self._power_per_bounce
 
     @power_per_bounce.setter
@@ -290,6 +454,11 @@ class Paddle:
 
     @property
     def power_multiplier(self):
+        """Returns the power bonus of the paddle.
+
+        Returns:
+            float: power bonus.
+        """
         return self._power_multiplier
 
     @power_multiplier.setter
@@ -298,6 +467,11 @@ class Paddle:
 
     @property
     def is_powered_up(self):
+        """Returns wether the paddle is powered up or not.
+
+        Returns:
+            bool: `True` if the paddle is powered up, `False` otherwise.
+        """
         return self._is_powered_up
 
     @is_powered_up.setter
@@ -306,9 +480,13 @@ class Paddle:
 
     @property
     def message_queue(self):
+        """Returns the message queue.
+
+        Returns:
+            dict: Message queue.
+        """
         return self._message_queue
 
     @message_queue.setter
     def message_queue(self, value):
         self._message_queue = value
-
