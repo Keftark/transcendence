e8072a86f61ea03dfb2cfbf5a0f0c9e2cb697cdb
MartinJuette <m.juette76@gmail.com>
2025-02-03 15:21:39 +0100

    Tournament bdd

--- djang/matchs/admin.py
+++ djang/matchs/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Match, Historique
+from .models import Match, Historique, TournamentMatchModel, TournamentModel
 
 admin.site.register(Match)
 admin.site.register(Historique)
+admin.site.register(TournamentModel)
+admin.site.register(TournamentMatchModel)
