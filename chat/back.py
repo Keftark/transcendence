e17d71873ef4b66b96e3d33774bafaac724d3dbf
Irina Nekotari <nolan.xx.mcfr@gmail.com>
2025-02-13 17:09:21 +0100

    Update back.py

--- chat/back.py
+++ chat/back.py
@@ -160,8 +160,10 @@ async def friends_handler(event):
                             await send_server(dumps.invite_friend(targ, user))
                         elif event["method"] == "accept":
                             await send_server(dumps.accept_friend(targ, user))
-                        else: #refuse
+                        elif event["method"] == "refuse":
                             await send_server(dumps.refuse_friend(targ, user))
+                        else: #cancel
+                            await send_server(dumps.cancel_friend(targ, user))
                         break
 
 async def chat_handler(event):
